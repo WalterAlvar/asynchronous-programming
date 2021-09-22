7977b94643b7f808fcfbe8aeeb9f5a1655eeffe5
Walter Alvarado <yovaemperor@gmail.com>
2021-09-22 19:29:09 +0200

    Update exercise-5.js

--- browser/isolate/1-event-loop/exercises/exercise-5.js
+++ browser/isolate/1-event-loop/exercises/exercise-5.js
@@ -5,7 +5,11 @@ const log = labeledLogger();
 // use setTimeout and .charCodeAt() to sort an array of strings
 //  read items from the unsorted array to the sorted array
 //  you can control the order of the sorted array by using different delays in a setTimeout
-const sortArray = (unsorted, sorted) => {};
+const sortArray = (unsorted, sorted) => {
+  sorted = toSort1
+  const index = 4
+  sorted.charCodeAt(index);
+};
 
 // can you figure out why the tests pass in the order they do?
 // is it possible for a test never to finish?
